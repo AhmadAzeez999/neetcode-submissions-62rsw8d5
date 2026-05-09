2d62058727098216c1764cafad88ef29c6b5ca3a
Ahmad Azeez <114791071+AhmadAzeez999@users.noreply.github.com>
2026-05-09 00:04:18 -0400

    Add: permutation-string - submission-7

---
+++ Data Structures & Algorithms/permutation-string/submission-7.py
@@ -0,0 +1,21 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2): return False
+
+        s1Count = [0] * 26
+        windowCount = [0] * 26
+
+        for c in s1:
+            s1Count[ord(c) - ord('a')] += 1
+
+        l = 0
+        for r in range(len(s2)):
+            windowCount[ord(s2[r]) - ord('a')] += 1
+
+            if r - l + 1 > len(s1):
+                windowCount[ord(s2[l]) - ord('a')] -= 1
+                l += 1
+
+            if s1Count == windowCount:
+                return True
+        return False
